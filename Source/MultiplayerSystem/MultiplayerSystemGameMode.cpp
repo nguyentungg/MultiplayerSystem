b1434c5100a5f0b110cf5343a3683e607b16edf5
Nguyen Tung <thanhtung4693@gmail.com>
2023-01-01 03:05:59 +0700

    Add new lobby game mode, add quit button for WBP_Menu,...

--- Source/MultiplayerSystem/MultiplayerSystemGameMode.cpp
+++ Source/MultiplayerSystem/MultiplayerSystemGameMode.cpp
@@ -7,7 +7,7 @@
 AMultiplayerSystemGameMode::AMultiplayerSystemGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacterUE4"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
